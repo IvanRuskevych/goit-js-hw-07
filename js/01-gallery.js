186ba5ef7b773f3d3c2e83fdbb7ad6d421f2ea26
Ivan Ruskevych <ivan.ruskevych@gmail.com>
2023-02-22 05:24:06 +0200

    task 07 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+console.log('galleryRef:', galleryRef);
+
+function createGalleryMarkup(arrItems) {
+  return arrItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`;
+    })
+    .join('');
+}
+
+galleryRef.innerHTML = createGalleryMarkup(galleryItems);
+// console.log(createGalleryMarkup(galleryItems));
+
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+
+  if (!e.target.hasAttribute('data-source')) {
+    return;
+  }
+  console.log(e.target);
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${e.target.getAttribute('data-source')}">
+`
+  );
+
+  instance.show(() => console.log('lightbox now visible'));
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+
 // Change code below this line
 
-console.log(galleryItems);
+const parentItemsRef = document.body.querySelector('.gallery');
+
+galleryItemsMarkup(galleryItems, parentItemsRef);
+
+function galleryItemsMarkup(arrItems, parentRef) {
+  const itemsMarkup = arrItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="rrrr" />
+                </a>
+             </div>`;
+    })
+    .join('');
+
+  return (parentRef.innerHTML = itemsMarkup);
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+console.log('SimpleLightbox:', SimpleLightbox);
